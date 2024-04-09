7c10c4314b52b0cb03eda354c66aa26487421440
alkaev <alkaev2004@gmail.com>
2024-04-09 21:43:11 +0300

    beginning

--- Cartesian_tree.cpp
+++ Cartesian_tree.cpp
@@ -4,26 +4,14 @@
 using namespace std;
 using namespace std::chrono;
 
-struct item {
-	int key, prior, cnt;
-	item * l, * r;
-	item() { }
-	item (int key, int prior) : key(key), prior(prior), l(NULL), r(NULL) { }
+struct Node {
+	int key, prior;
+	Node * l, * r;
+	Node() { }
+	Node (int key, int prior) : key(key), prior(prior), l(NULL), r(NULL) { }
 };
 
-typedef item * pitem;
-
-int cnt (pitem t) {
-	return t ? t->cnt : 0;
-}
-
-void upd_cnt (pitem t) {
-	if (t)
-		t->cnt = 1 + cnt(t->l) + cnt (t->r);
-}
-
-
-void split (pitem t, int key, pitem & l, pitem & r) {
+void split (Node* t, int key, Node* & l, Node* & r) {
 	if (!t)
 		l = r = NULL;
 	else if (key < t->key)
@@ -33,7 +21,7 @@ void split (pitem t, int key, pitem & l, pitem & r) {
 }
 
 
-void insert (pitem & t, pitem it) {
+void insert (Node* & t, Node* it) {
 	if (!t)
 		t = it;
 	else if (it->prior > t->prior)
@@ -42,7 +30,7 @@ void insert (pitem & t, pitem it) {
 		insert (it->key < t->key ? t->l : t->r, it);
 }
 
-void merge (pitem & t, pitem l, pitem r) {
+void merge (Node* & t, Node* l, Node* r) {
 	if (!l || !r)
 		t = l ? l : r;
 	else if (l->prior > r->prior)
@@ -51,34 +39,34 @@ void merge (pitem & t, pitem l, pitem r) {
 		merge (r->l, l, r->l),  t = r;
 }
 
-void erase (pitem & t, int key) {
+void erase (Node* & t, int key) {
 	if (t->key == key)
 		merge (t, t->l, t->r);
 	else
 		erase (key < t->key ? t->l : t->r, key);
 }
 
-pitem unite (pitem l, pitem r) {
+Node* unite (Node* l, Node* r) {
 	if (!l || !r)  return l ? l : r;
 	if (l->prior < r->prior)  swap (l, r);
-	pitem lt, rt;
+	Node* lt, *rt;
 	split (r, l->key, lt, rt);
 	l->l = unite (l->l, lt);
 	l->r = unite (l->r, rt);
 	return l;
 }
 
-bool search(pitem t, int x) {
+bool search(Node* t, int x) {
     if (!t) return false;
     if (t->key == x) return true;
     if (x < t->key) return search(t->l, x);
     else return search(t->r, x);
 }
 
-pitem build(int* keys, int* priors, int n) {
-    pitem t = NULL;
+Node* build(int* keys, int* priors, int n) {
+    Node* t = NULL;
     for (int i = 0; i < n; ++i) {
-        pitem new_item = new item(keys[i], priors[i]);
+        Node* new_item = new Node(keys[i], priors[i]);
         insert(t, new_item);
     }
     return t;
@@ -95,9 +83,9 @@ int main() {
 
 
     // Строим дерево
-    pitem root = NULL;
+    Node* root = NULL;
     for (int i = 0; i < n; i++) {
-        insert(root, new item(keys[i], priors[i]));
+        insert(root, new Node(keys[i], priors[i]));
     }
 
     // Выполним поиск нескольких ключей
