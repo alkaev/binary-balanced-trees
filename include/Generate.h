813671a7498f956d2a9549ecf4aef3b37ebed958
alkaev <alkaev2004@gmail.com>
2024-05-19 01:06:03 +0300

    delete_files

--- include/Generate.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATA_GENERATOR_H
-#define DATA_GENERATOR_H
-
-#include <fstream>
-#include <string>
-
-void generateDataFile(const std::string& filename, int count, const std::string& structure);
-
-#endif // DATA_GENERATOR_H
